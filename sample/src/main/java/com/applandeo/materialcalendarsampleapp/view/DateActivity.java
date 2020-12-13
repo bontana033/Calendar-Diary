df4eb4287373da849679c5487c352a15c84dc9be
Junwoo <blackrabit.0225@gmail.com>
2020-12-14 07:14:13 +0900

    DateActivity 에서 날짜 텍스트를 클릭하면 RangePicker 액티비티가 나오도록 했습니다.

--- sample/src/main/java/com/applandeo/materialcalendarsampleapp/view/DateActivity.java
+++ sample/src/main/java/com/applandeo/materialcalendarsampleapp/view/DateActivity.java
@@ -2,11 +2,16 @@ package com.applandeo.materialcalendarsampleapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.applandeo.materialcalendarsampleapp.BlankActivity;
 import com.applandeo.materialcalendarsampleapp.R;
+import com.applandeo.materialcalendarsampleapp.RangePickerActivity;
 
 public class DateActivity extends BlankActivity {
     /**
@@ -22,5 +27,11 @@ public class DateActivity extends BlankActivity {
         btn_ok.setOnClickListener(l -> finish() );
         Button btn_cancel = findViewById(R.id.btn_cancel);
         btn_cancel.setOnClickListener(l -> finish() );
+
+        TextView t_dateRange = findViewById(R.id.t_dateRange);
+        t_dateRange.setOnClickListener(view -> {
+            Intent intent = new Intent(DateActivity.this, RangePickerActivity.class);
+            startActivityForResult(intent, 0);
+        });
     }
 }
